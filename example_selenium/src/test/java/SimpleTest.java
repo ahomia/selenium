8cd0681d98dd3f4d9acc79853a0a601b8afc0b97
ahomia <marisa_mam@mail.ru>
2017-05-23 23:47:47 +0400

    ex2

--- example_selenium/src/test/java/SimpleTest.java
+++ example_selenium/src/test/java/SimpleTest.java
@@ -1,4 +1,8 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -7,14 +11,36 @@ import java.util.concurrent.TimeUnit;
  * Created by axomia on 23.05.2017.
  */
 public class SimpleTest {
-    @Test
-    public void simpleTest(){
-        FirefoxDriver wd;
-        wd=new FirefoxDriver();
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        wd.get("https://github.com/");
-        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-        wd.quit();
+    WebDriver wd;
 
+    @Before
+    public void init() {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        wd.get("http://localhost/litecart/admin/.");
+    }
+
+    public void login(String username, String password) {
+        wd.findElement(By.name("username")).click();
+        wd.findElement(By.name("username")).clear();
+        wd.findElement(By.name("username")).sendKeys(username);
+        wd.findElement(By.name("password")).click();
+        wd.findElement(By.name("password")).clear();
+        wd.findElement(By.name("password")).sendKeys(password);
+        wd.findElement(By.name("login")).click();
+        wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+
+    @Test
+    public void simpleTest() {
+        login("admin", "admin");
+
+
+    }
+
+   @After
+    public void stop() {
+        wd.quit();
     }
 }
